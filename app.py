3270bd23c9a87e829e76320266b23580e85daf9f
David Crocker <davidwcrocker@gmail.com>
2017-07-30 00:58:18 +0000

    Turned off modification tracking

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config["SECRET_KEY"] = config.APP_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = config.DEV_DB
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
